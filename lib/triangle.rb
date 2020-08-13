191dc674184c44c2c5d231cea3134656319e6efe
Nerly Ton <nerlyton@gmail.com>
2020-08-13 04:02:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,22 +8,25 @@ class Triangle
   end
   
   def kind 
-    if (length_side_a <= 0) || (length_side_b <= 0) || (length_side_c <= 0)
+    if (@length_side_a <= 0) || (@length_side_b <= 0) || (@length_side_c <= 0)
       raise TriangleError 
       elsif
-      (length_side_a+length_side_b <= length_side_c) || (length_side_a+length_side_c <= length_side_b) || (length_side_b+length_side_c <= length_side_a)
+      (@length_side_a+@length_side_b <= @length_side_c) || (@length_side_a+@length_side_c <= @length_side_b) || (@length_side_b+@length_side_c <= @length_side_a)
         raise TriangleError
       else 
-        if length_side_a == length_side_b == length_side_c
+        if @length_side_a == @length_side_b == @length_side_c
       :equilateral
     elsif
-      length_side_a == length_side_b || length_side_b == length_side_c || length_side_a == length_side_c
+      @length_side_a == @length_side_b || @length_side_b == @length_side_c || @length_side_a == @length_side_c
       :isosceles
     elsif
-      length_side_a != length_side_b != length_side_c
+      @length_side_a != @length_side_b != @length_side_c
       :scalene
     end
   end
+end
+
+class TriangleError < StandardError
 end
 
       
