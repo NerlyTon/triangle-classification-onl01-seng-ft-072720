f9eeaf9ed09fa43a1d865a6cad0a553b6ac253f9
Nerly Ton <nerlyton@gmail.com>
2020-08-13 04:11:53 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_accessor :length_side_a, :length_side_b, :length_side_c
+  
+  def initialize(length_side_a, length_side_b, length_side_c)
+    @length_side_a = length_side_a
+    @length_side_b = length_side_b
+    @length_side_c = length_side_c
+  end
+  
+  def kind 
+    if (@length_side_a <= 0) || (@length_side_b <= 0) || (@length_side_c <= 0)
+      raise TriangleError 
+      elsif
+      (@length_side_a+@length_side_b <= @length_side_c) || (@length_side_a+@length_side_c <= @length_side_b) || (@length_side_b+@length_side_c <= @length_side_a)
+        raise TriangleError
+      end
+      
+      if @length_side_a == @length_side_b && @length_side_b == @length_side_c && @length_side_a == @length_side_c
+      :equilateral
+    elsif
+      @length_side_a != @length_side_b && @length_side_b != @length_side_c && @length_side_a != @length_side_c
+      :scalene
+    else
+      @length_side_a == @length_side_b || @length_side_b == @length_side_c || @length_side_a == @length_side_c
+      :isosceles
+    end
+  end
+
+
+class TriangleError < StandardError
+end
+
+      
+      
 end
